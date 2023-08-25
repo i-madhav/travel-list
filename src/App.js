60f06d13cfb95e3124588fb2ef2b8db29c75eba9
Madhav <madhav.shar06ma@gmail.com>
2023-08-25 14:48:19 +0530

    g

--- src/App.js
+++ src/App.js
@@ -23,23 +23,28 @@ function Logo(){
 
 function Form(){
 const[description,setDescription]=useState("")
-const[quantity,setQuantity]=useState(5)
+const[quantity,setQuantity]=useState(1)
 
 function handleSubmit(e){
   e.preventDefault();
+  if(!description) return null;
+  const newItem = {description, id:Date.now() ,packed:false , quantity};
+  console.log(newItem)
+
+  setDescription("")
+  setQuantity(1)
 }
   return(
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip!</h3>
-      <select value={quantity} onChange={(e)=>setQuantity(e.target.value)}>
+      <select value={quantity} onChange={(e)=>setQuantity(Number(e.target.value))}>
         {Array.from({length:30},(_,i)=>i+1)
         .map(num=> <option value = {num} key={num}>
           {num}
         </option>)}
       </select>
      
-      <input type="text" placeholder="...Likho Kuch"  value={description} onChange={(e)=>{console.log(e.target.value)
-       setDescription(e.target.value)}}/>
+      <input type="text" placeholder="...Likho Kuch"  value={description} onChange={(e)=>{setDescription(e.target.value)}}/>
       <button>Add</button>
     </form>
   )
