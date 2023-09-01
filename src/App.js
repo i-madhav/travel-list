c3c29eed1e414cba9ef1d4a80bbba220a253fd5e
Madhav <madhav.shar06ma@gmail.com>
2023-09-01 17:27:18 +0530

    vdf

--- src/App.js
+++ src/App.js
@@ -68,12 +68,25 @@ function handleSubmit(e){
 }
 
 function PackingList({itemsData , onDeleteItem , onToggleItem}){
+  const[sortBy , setSortBy] = useState("input")
+  let sortItem;
+  if(sortBy === "input") sortItem = itemsData;
+  if(sortBy === "description") sortItem = itemsData.slice().sort((a,b)=>a.description.localeCompare(b.description));
+  if(sortBy === "packed") sortItem = itemsData.slice().sort((a,b)=>Number(a.packed)-Number(b.packed))
   return(
     <div className="list">
     <ul>
     {itemsData.map((item)=>
      <Item itemObj ={item} key={item.id} onDeleteItem={ onDeleteItem} onToggleItem={onToggleItem} />)}
     </ul>
+
+    <div className="actions">
+      <select value={sortBy} onChange={(e)=>setSortBy(e.target.value)}>
+        <option value='input'>Sort by input order</option>
+        <option value='description'>Sort by description</option>
+        <option value='packed'>Sort by packed status</option>
+      </select>
+    </div>
   </div>
 )}
 
